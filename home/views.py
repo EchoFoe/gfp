e43a90a80707c4cb91daa7e3da6357b7f62cbd7a
EchoFoe <Aspirpd@gmail.com>
2020-01-22 23:51:35 +0300

    tournaments.order_by 'start_time' in home.views

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import *
+# from sportsman.models import *
 from tournaments.models import *
 from reviews.models import *
 from documents.models import *
@@ -7,7 +8,7 @@ from documents.models import *
 
 def home(request):
     all_tournaments = TournamentImage.objects.filter(is_active=True, is_main=True, tournament__is_active=True)
-    new_tournaments = all_tournaments.filter(tournament__status_id=1)
+    new_tournaments = all_tournaments.filter(tournament__status_id=1).order_by('tournament__start_time')
     held_tournaments = all_tournaments.filter(tournament__status_id=2)
     canceled_tournaments = all_tournaments.filter(tournament__status_id=3)
     failed_tournaments = all_tournaments.filter(tournament__status_id=4)
